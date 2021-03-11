79a448f967f96e6e7128e456d282a717774d3f4c
Iago Ronan <iago.santos@privatecode.com.br>
2021-03-11 10:37:10 -0300

    nova forma de obter feed

--- src/Instagram.php
+++ src/Instagram.php
@@ -6,21 +6,25 @@ class Instagram {
     private $profile;
 
     function __construct($account) {
-        $response = file_get_contents("https://www.instagram.com/$account/");
+        $ch = curl_init();
 
-        $data = explode("window._sharedData = ", $response)[1];
+        curl_setopt($ch, CURLOPT_URL, "https://www.instagram.com/graphql/query/?query_hash=003056d32c2554def87228bc3fd9668a&variables=%7B%22id%22%3A%22".$account."%22%2C%22first%22%3A12%2C%22after%22%3A%22QVFCSmQ3SE56OFllMUJhdlM5TEFBT19sU0hBS3Q4UzJIbkdTMjhtRUluYWlpYllNTGF2NHV0UFY1VGFDMGo0bG5jcm9BWnh4Vk9vNDN5SjJSem9pZXhXQg%3D%3D%22%7D");
 
-        $data = explode("\x3c/script>", $data)[0];
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-        $data = str_replace(";", "", $data);
+        $response = curl_exec($ch);
 
-        $this->profile = json_decode($data);
+        curl_close($ch);
+
+        $response = json_decode($response);
+        
+        $this->profile = json_decode($response);
     }
 
     public function feed() {
         $arr = [];
 
-        $posts =  $this->profile->entry_data->ProfilePage[0]->graphql->user->edge_owner_to_timeline_media->edges;
+        $posts = $response->data->user->edge_owner_to_timeline_media->edges;
 
         foreach($posts as $post) {
 
@@ -28,7 +32,7 @@ class Instagram {
                 "image" => $post->node->display_url,
                 "description" => $post->node->edge_media_to_caption->edges[0]->node->text,
                 "date" => $post->node->taken_at_timestamp,
-                "likes" => $post->node->edge_liked_by->count,
+                "likes" => $post->node->edge_media_preview_like->count,
                 "comments" => $post->node->edge_media_to_comment->count,
                 "url" => 'https://www.instagram.com/p/'.$post->node->shortcode.'/',
             ];
